b687a4022a986990b229a5358fc3668408ca41bb
Jeremy-Qian <jeremytheking2013@gmail.com>
2026-06-23 08:24:24 +0800

    Added meaningful comments to the fs crate

--- crates/fs/src/lib.rs
+++ crates/fs/src/lib.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+/// Manages the files
 pub struct FileManager {
     pub current_path: Option<PathBuf>,
 }
@@ -9,6 +10,7 @@ impl FileManager {
         Self { current_path: None }
     }
 
+    /// Opens files dialog and then returns its path
     pub fn open_file_dialog() -> Option<(String, PathBuf)> {
         let file = rfd::FileDialog::new().set_title("Open File").pick_file()?;
 
@@ -16,20 +18,25 @@ impl FileManager {
         Some((content, file))
     }
 
+    /// Opens save dialog
     pub fn save_as_dialog() -> Option<PathBuf> {
         rfd::FileDialog::new().set_title("Save File").save_file()
     }
 
+    /// Saves content to path
     pub fn save_to_path(content: &str, path: &PathBuf) -> Result<(), std::io::Error> {
+        // TODO: Add proper error handling
         std::fs::write(path, content)
     }
 
+    /// Returns display name, like main.rs instead of user/project/main.rs
     pub fn display_name(&self) -> String {
         self.current_path
             .as_ref()
             .and_then(|p| p.file_name())
             .and_then(|n| n.to_str())
             .map(|s| s.to_string())
+            // Or returns Untitled
             .unwrap_or_else(|| "Untitled".to_string())
     }
 }
